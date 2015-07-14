4d8479d17b07918fcca223cf68070bc77891d9fe
UelitonFreitas <freitas.ueliton@gmail.com>
2015-07-13 22:24:11 -0300

    Primeira versão de detecção da mão. Testar no escuro com iluminação direcionada para mão

--- cam_joken_po.py
+++ cam_joken_po.py
@@ -9,8 +9,8 @@ Joken po com a camera
 #Camera
 cap = cv2.VideoCapture(0)
 
-cv2.namedWindow('frame1')
-cv2.namedWindow('frame2')
+# cv2.namedWindow('frame1')
+# cv2.namedWindow('frame2')
 min = 0
 max = 400
 
@@ -27,21 +27,37 @@ def cannyTreshouldMax(value):
     global cannyMax
     cannyMax = value
 
-#Barras
-cv2.createTrackbar('Trashold-Min', 'frame1', min, max, cannyTreshouldMin)
-cv2.createTrackbar('Trashold-Max', 'frame1', min, max, cannyTreshouldMax)
 
 while(True):
     # frame-By-Frame
     ret, frame = cap.read()
 
     # Operacores com os frames
+    #Imagem em preto e branco
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canny = cv2.Canny(gray, cannyMin, cannyMax)
-    print "Min:"+str(cannyMin)+" Max:"+str(cannyMax)
-    # Resultados
-    cv2.imshow('frame1',canny)
-    cv2.imshow('frame2',gray)
+    #remove ruidos com a gaussiana
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    #aplica um trasehold...está invertido para ser utilizado no escuro para remover o fundo com facilidade
+    ret, thresh1 = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #encontra os contornos da mão.
+    ret, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #encontra o contorno de maior area
+    max_area = 0.0
+    for i in range(len(contours)):
+            cnt = contours[i]
+            area = cv2.contourArea(cnt)
+            if(area > max_area):
+                max_area=area
+                ci=i
+    cnt=contours[ci]
+    #desenha os contornos
+    hull = cv2.convexHull(cnt)
+    drawing = np.zeros(frame.shape, np.uint8)
+    cv2.drawContours(drawing,[cnt],0,(0,255,0),2)
+    cv2.drawContours(drawing,[hull],0,(0,0,255),2)
+
+    cv2.imshow("Original", frame)
+    cv2.imshow('Result', drawing)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
